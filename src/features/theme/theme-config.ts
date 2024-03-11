1ac1457e335114d0d13224eff57d5763a30e9e60
Alexis Guichard <alexis.guichard@ratpsmartsystems.com>
2024-03-11 11:46:16 +0000

    fix style

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,7 +1,7 @@
-export const AI_NAME = "RATP Smart Systems Chat";
+export const AI_NAME = "RATP Smart Systems";
 export const AI_DESCRIPTION = `Bienvenue sur l'interface ChatGPT, le foyer de vos assistants virtuels. \nProfitez de la puissance de l'IA pour répondre à vos questions, comprendre des images ou des documents, stocker vos prompts favoris, le tout en un seul endroit facile d'accès. ChatGPT est là pour vous aider à gagner du temps, améliorer votre productivité et vous permettre de vous concentrer sur des tâches plus importantes !`;
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `Tu es un assistant de ${AI_NAME}. Tu dois toujours renvoyer en format markdown.`;
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `Tu dois toujours renvoyer en format markdown.`;
 
 export const NEW_CHAT_NAME = "New chat";
