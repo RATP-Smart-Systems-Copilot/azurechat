495720b3b1fb6ec8a13bf0adb80409592900d4af
Alexis Guichard <alexis.guichard@ratpsmartsystems.com>
2024-03-11 11:48:51 +0000

    update default prompt message

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -2,6 +2,9 @@ export const AI_NAME = "RATP Smart Systems";
 export const AI_DESCRIPTION = `Bienvenue sur l'interface ChatGPT, le foyer de vos assistants virtuels. \nProfitez de la puissance de l'IA pour répondre à vos questions, comprendre des images ou des documents, stocker vos prompts favoris, le tout en un seul endroit facile d'accès. ChatGPT est là pour vous aider à gagner du temps, améliorer votre productivité et vous permettre de vous concentrer sur des tâches plus importantes !`;
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `Tu dois toujours renvoyer en format markdown.`;
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `Tu es un assistant d'IA. Tu dois toujours répondre dans le format markdown.
+
+Tu as accès aux fonctions suivantes:
+1. create_img: Tu dois toujours utiliser cette fonction create_img si l'utilisateur te demande de créer une image.`;
 
 export const NEW_CHAT_NAME = "New chat";
