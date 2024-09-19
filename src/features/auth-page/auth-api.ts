07ef4e48b72d773cb6d022735d253e25470767ef
aguichardrss <157721302+aguichardrss@users.noreply.github.com>
2024-09-19 16:58:55 +0200

    revert (#25)

--- src/features/auth-page/auth-api.ts
+++ src/features/auth-page/auth-api.ts
@@ -38,7 +38,6 @@ const configureIdentityProvider = () => {
         clientId: process.env.AZURE_AD_CLIENT_ID!,
         clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
         tenantId: process.env.AZURE_AD_TENANT_ID!,
-        authorization: { params: { scope: "openid profile User.Read email" } },
         async profile(profile) {
           const newProfile = {
             ...profile,
