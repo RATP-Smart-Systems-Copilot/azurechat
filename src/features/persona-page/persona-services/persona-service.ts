2eeddb22d8ad6e463edf062f76cb58a6d3a11b01
aguichardrss <157721302+aguichardrss@users.noreply.github.com>
2025-02-27 15:16:03 +0100

    Revert "add mistral (#36)"
    This reverts commit c6f36ba81f7fbe44bba570b2e42086b1ac30f733.

--- src/features/persona-page/persona-services/persona-service.ts
+++ src/features/persona-page/persona-services/persona-service.ts
@@ -15,10 +15,9 @@ import { HistoryContainer } from "@/features/common/services/cosmos";
 import { uniqueId } from "@/features/common/util";
 import { SqlQuerySpec } from "@azure/cosmos";
 import { PERSONA_ATTRIBUTE, PersonaModel, PersonaModelSchema } from "./models";
-import { defaultGPTModel } from "@/features/common/services/openai";
+import { defaultGPTModel, modelOptions } from "@/features/common/services/openai";
 import { FindAllChatDocumentsByPersona } from "@/features/chat-page/chat-services/chat-document-service";
 import { DeleteDocumentsForPersona } from "@/features/chat-page/chat-services/azure-ai-search/azure-ai-search";
-import { modelOptions } from "@/features/theme/theme-config";
 
 interface PersonaInput {
   name: string;
