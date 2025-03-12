4ee416d300e47fbd26fd9ccc43e0f303c569bd36
Alexis Guichard <alexis.guichard@ratpsmartsystems.com>
2025-03-12 12:51:19 +0000

    resolve error image shown

--- src/features/chat-page/chat-services/images-api.ts
+++ src/features/chat-page/chat-services/images-api.ts
@@ -6,10 +6,13 @@ import {
 export const ImageAPIEntry = async (request: Request): Promise<Response> => {
   const urlPath = request.url;
 
-  const response = GetThreadAndImageFromUrl(urlPath);
+  const response = await GetThreadAndImageFromUrl(urlPath);
 
   if (response.status !== "OK") {
-    return new Response(response.errors[0].message, { status: 404 });
+    const errorMessage = response.errors && response.errors.length > 0
+    ? response.errors[0].message
+    : "Une erreur inconnue s'est produite.";
+    return new Response(errorMessage, { status: 404 });
   }
 
   const { threadId, imgName } = response.response;
